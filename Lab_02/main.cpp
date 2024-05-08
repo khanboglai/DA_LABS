28594828a595da0bff9713661d7c36b91e6c36e8
Aleksey <boglaev.ale@gmail.com>
2024-05-08 09:22:14 +0300

    Rewrite btree and fix bugs

--- Lab_02/main.cpp
+++ Lab_02/main.cpp
@@ -1,11 +1,43 @@
-#include "BT.hpp"
+#include "btree.hpp"
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+#include <set>
 
+int main()
+{
+    BTree b;
 
-int main() {
-    BT<int> t(2);
-    t.Insert(3);
-    t.Insert(1);
-    t.Insert(2);
-    t.Insert(0);
-    t.Print();
+    std::string in;
+    while (std::cin >> in) {
+        if (in == "+") {
+            int val;
+            std::cin >> val;
+            if (!b.Search(val)) {
+                b.Insert(val);
+                std::cout << "OK" << std::endl;
+            } else {
+                std::cout << "Exist" << std::endl;
+            }
+        } else if (in == "-") {
+            int val;
+            std::cin >> val;
+            bool res = b.Delete(val);
+            if (res) {
+                std::cout << "OK" << std::endl;
+            } else {
+                std::cout << "NoSuchWord" << std::endl;
+            }
+        }
+    }
+
+    // b.Insert(5);
+    // b.Insert(6);
+    // b.Insert(7);
+    // b.Insert(4);
+
+    // b.Display();
+
+    // b.Delete(6);
+    // b.Display();
 }
---
+++ Lab_02/wrapper.sh
@@ -0,0 +1,80 @@
+#! /usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+readonly TESTS_DIR=tests
+
+function log_info()
+{
+  log_ "info" "$@"
+}
+
+function log_error()
+{
+  log_ "error" "$@"
+}
+
+function log_()
+{
+  local type=$1
+  local message=$2
+  local date_str=; date_str=$(date +'%Y-%m-%d %H:%M:%S')
+  echo "[${type}] [${date_str}] ${message}"
+}
+
+function main()
+{
+  local bin=lab2
+  log_info "Stage #1. Compiling..."
+  if ! make ; then
+    log_error "Failed to compile lab2.cpp"
+    return 1
+  fi
+  log_info "Compiling OK"
+
+  log_info "Stage #2. Test generating..."
+  rm -rf ${TESTS_DIR}
+  mkdir ${TESTS_DIR}
+  local count_of_tests=3
+  if ! ./gen.py ${TESTS_DIR} ${count_of_tests} ; then
+    log_error "Failed to generate tests"
+    return 1
+  fi
+  log_info "Generating OK"
+
+  log_info "Stage #3 Checking..."
+  for test_file in $( ls ${TESTS_DIR}/*.t ) ; do
+    local tmp_output=tmp
+    if ! ./${bin} < ${test_file} > ${tmp_output} ; then
+      log_error "Failed to run test"
+      return 1
+    fi
+    local file_line_cnt=; file_line_cnt=$(cat ${test_file} | wc -l | sed -e 's/ *//g')
+    local answer_file=${test_file%.*}.a
+    if ! diff -u ${answer_file} ${tmp_output} ; then
+      log_error "Failed to check test ${test_file}."
+      return 1
+    fi
+    log_info "${test_file}, lines=${file_line_cnt} OK"
+  done
+
+  log_info "Stage #4 Benchmarking..."
+  #if ! make benchmark ; then
+  #  log_info "Failed to compile benchmark."
+  #  return 1
+  #fi
+  #local benchmark_bin=./benchmark
+  #for test_file in $( ls ${TESTS_DIR}/*.t ) ; do
+  #  log_info "Running ${test_file}"
+  #  if ! ${benchmark_bin} < ${test_file} ; then
+  #    log_error "Failed to run ${benchmark_bin} for ${test_file}."
+  #    return 1
+  #  fi
+  #done
+}
+
+# $1 -- первый аргумент
+# %2 -- второй аргумент
+# $@ -- все аргументы
+main $@
