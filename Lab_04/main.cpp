27147c152f139989690fa5f61ea0436963d6730b
Aleksey <boglaev.ale@gmail.com>
2024-05-29 17:27:39 +0300

    Start lab

---
+++ Lab_04/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+
+const uint32_t SEP = -1; // разделитель
+using TDigitSRH = std::pair<uint32_t, std::pair<size_t, size_t>>; // псевдоним, чтобы меньше писать
+
+
+std::vector<int> Z_func(const std::vector<TDigitSRH>& S) {
+    int n = S.size();
+    int l = 0, r = 0;
+    std::vector<int> Z(n);
+
+    for (int i = 1; i < n; i++) {
+        
+        Z[i] = std::max(0, std::min(r - i, Z[i - l]));
+
+        while (i + Z[i] < n && S[Z[i]].first == S[i + Z[i]].first) { // это стандартный подсчет z-функции
+            Z[i]++;
+        }
+        
+        if (i + Z[i] > r) {
+            l = i;
+            r = i + Z[i];
+        }
+    }
+
+    return Z;
+}
+
+
+int main() {
+    std::string P;
+    getline(std::cin, P);
+    std::vector<TDigitSRH> vec_p;
+    
+    // подготовка паттерна
+    std::istringstream ss(P);
+    uint32_t num = 0;
+    size_t cnt_num = 0;
+    while (ss >> num) {
+        vec_p.push_back(std::make_pair(num, std::make_pair(cnt_num, 0)));
+        cnt_num++;
+    }
+
+    int p_sz = vec_p.size();
+    TDigitSRH sep_elem = std::make_pair(SEP, std::make_pair(vec_p.size(), 1));
+
+    vec_p.push_back(sep_elem);
+
+    // подготовка текста
+    std::string T;
+    size_t cnt_line = 1;
+    
+    while (getline(std::cin, T)) {
+        
+        std::istringstream ss(T);
+        uint32_t num = 0;
+        size_t cnt_num = 0;
+        while (ss >> num) {
+            vec_p.push_back(std::make_pair(num, std::make_pair(cnt_num, cnt_line)));
+            cnt_num++; // номер слова
+        }
+        cnt_line++; // номер строки
+    }
+
+
+    std::vector<int> Z = Z_func(vec_p);
+
+    for (size_t i = 0; i < Z.size(); i++) {
+        if (Z[i] == p_sz) {
+            std::cout << vec_p[i].second.second << ", " << vec_p[i].second.first + 1 << std::endl;
+        }
+    }
+
+    // for (int elem : Z) {
+    //     std::cout << elem << " ";
+    // }
+    // std::cout << std::endl;
+}
