260a8e334d0bc5992bc1a5ef338c8796a094c13e
Aleksey <boglaev.ale@gmail.com>
2024-05-30 11:26:17 +0300

    Fix report

--- Lab_04/bechmark.cpp
+++ Lab_04/bechmark.cpp
@@ -9,6 +9,28 @@ const int SEP = -1; // разделитель
 using TDigitSRH = std::pair<int64_t, std::pair<size_t, size_t>>; // псевдоним, чтобы меньше писать
 
 
+std::vector<int> prefix_function(const std::vector<TDigitSRH>& s) {
+    std:: vector<int> pi(s.size(), 0);
+
+    for (int i = 1; i < (int)s.size(); i++) {
+        int j = pi[i - 1];
+
+        while (j > 0 && s[i].first != s[j].first) {
+            j = pi[j - 1];
+        }
+
+        if (s[i].first == s[j].first) {
+            pi[i] = j + 1;
+        } else {
+            pi[i] = j;
+        }
+    }
+
+    return pi;
+}
+
+
+
 std::vector<int> Z_func_naive(const std::vector<TDigitSRH>& S) {
     int n = S.size();
     std::vector<int> Z(n, 0);
@@ -51,6 +73,7 @@ int main() {
 
     long double Z_naive = 0;
     long double Z_eff = 0;
+    long double KMP = 0;
     long double start, end;
 
     std::string P;
@@ -66,7 +89,7 @@ int main() {
         cnt_n++;
     }
 
-    
+    //int t_len = vec_p.size();
     TDigitSRH sep_elem = std::make_pair(SEP, std::make_pair(vec_p.size(), 1));
 
     vec_p.push_back(sep_elem);
@@ -99,12 +122,32 @@ int main() {
     Z_naive = Z_naive + end - start;
 
 
+    start = clock();
+    std::vector<int> pi = prefix_function(vec_p);
+    end = clock();
+    KMP = KMP + end - start;
+
+    // for (int i = 0; i < (int)vec_p.size() - t_len - 1; i++) {
+    //     if (pi[t_len + 1 + i] == t_len) {
+    //         std::cout << "s[" << i - t_len + 1 << ".." << i << "] = t" << std::endl;
+    //     }
+    // }
+
+
     std::cout << "Z naive: " << std::fixed << std::setprecision(3) << Z_naive / 1000.0 << " ms"<< std::endl;
     std::cout << "Z effective: " << std::fixed << std::setprecision(3) << Z_eff / 1000.0 << " ms" << std::endl;
+    std::cout << "KMP: " << std::fixed << std::setprecision(3) << KMP / 1000.0 << " ms" << std::endl;
 
+    // std::vector<int> Z_naive_r;
+    // std::vector<int> Z_r;
     // for (size_t i = p_sz + 1; i < Z.size(); i++) {
     //     if (Z[i] == p_sz) {
-    //         std::cout << vec_p[i].second.second << ", " << vec_p[i].second.first + 1 << std::endl;
+    //         Z_naive_r.push_back(vec_p[i].second.first + 1);
+    //         Z_r.push_back(vec_p[i].second.first + 1);
     //     }
+    // }
+
+    // for (size_t i = 0; i < Z_r.size(); i++) {
+    //     std::cout << Z_r[i] << " " << Z_naive_r[i] << " " << kmp_res[i] << std::endl;
     // }
 }
