8e783548ca4d5fc6034c18cd2010457018504d64
Aleksey <boglaev.ale@gmail.com>
2024-10-01 19:20:06 +0300

    Нихуя не получается сделать

--- Lab_05/main.cpp
+++ Lab_05/main.cpp
@@ -20,13 +20,6 @@ int main() {
     std::cin >> text;
     t.MatchStatistic(ms, text);
 
-    // std::cout << str1 << std::endl;
-
-    // for (char el : text) {
-    //     std::cout << el << " ";
-    // }
-    // std::cout << std::endl;
-
     for (size_t i = 0; i < ms.size(); i++) {
         std::cout << ms[i] << " "; 
     }
